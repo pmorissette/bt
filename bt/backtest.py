ab0a09eec90d91189db94a9ca6ee93bfd9734b39
Philippe Morissette <pm@rdacap.com>
2014-04-03 15:21:07 -0400

    added weights to Backtest and also added Result which inherits from GroupStatistics

--- bt/backtest.py
+++ bt/backtest.py
@@ -29,6 +29,11 @@ class Backtest(object):
         if commissions:
             self.strategy.set_commissions(commissions)
 
+        self.stats = {}
+        self._original_prices = None
+        self._weights = None
+        self._sweights = None
+
     def run(self):
         # setup strategy
         self.strategy.setup(self.data)
@@ -40,3 +45,50 @@ class Backtest(object):
         for dt in self.dates:
             self.strategy.update(dt)
             self.strategy.run()
+            # need update after to save weights, values and such
+            self.strategy.update(dt)
+
+        self.stats = self.strategy.prices.calc_perf_stats()
+        self._original_prices = self.strategy.prices
+
+    @property
+    def weights(self):
+        if self._weights is not None:
+            return self._weights
+        else:
+            vals = pd.DataFrame({x.full_name: x.values for x in
+                                 self.strategy.members})
+            vals = vals.div(self.strategy.values, axis=0)
+            self._weights = vals
+            return vals
+
+    @property
+    def security_weights(self):
+        if self._sweights is not None:
+            return self._sweights
+        else:
+            # get values for all securities in tree and divide by root values
+            # for security weights
+            vals = pd.DataFrame({x.name: x.values for x in
+                                 self.strategy.members if
+                                 isinstance(x, bt.core.SecurityBase)})
+            vals = vals.div(self.strategy.values, axis=0)
+
+            # combine securities with same ticker
+            vals = vals.groupby(vals.columns, axis=1).sum()
+
+            # save for future use
+            self._sweights = vals
+
+            return vals
+
+
+class Result(ffn.GroupStats):
+
+    def __init__(self, *backtests):
+        tmp = [pd.DataFrame({x.name: x.strategy.prices}) for x in backtests]
+        super(Result, self).__init__(*tmp)
+        self.backtests = {x.name: x for x in backtests}
+
+    def set_date_range(self, start=None, end=None):
+        self.stats.set_date_range(start, end)
