4c829cbfe7501f98f1209065b170312f0dbf9f07
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2025-02-11 16:16:27 -0500

    Bump ffn to 1.1.2 in runtime deps too

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     keywords="python finance quant backtesting strategies algotrading algorithmic trading",
     url="https://github.com/pmorissette/bt",
     license="MIT",
-    install_requires=["ffn>=1.1.0", "pyprind>=2.11", "tqdm>=4"],
+    install_requires=["ffn>=1.1.2", "pyprind>=2.11", "tqdm>=4"],
     extras_require={
         "dev": [
             "cython>=0.29.25",
